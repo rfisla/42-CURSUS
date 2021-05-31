b3ed6870f4d12ac65eb2d38cc2d86ac1ab8f26c1
rfisla <rubenfernandezisla@gmail.com>
2021-05-31 23:48:01 +0200

    first steps with ft_split.c

--- LIBFT/ft_split.c
+++ LIBFT/ft_split.c
@@ -1,9 +1,87 @@
 #include "libft.h"
 
-/*char **ft_split(char const *s, char c)
+static int count_words (char const *s, char c)
 {
-    char    **tabstr;
+    int     words;
     int     i;
 
+    words = 0;
+    i = 0;
+    if (!s)
+        return (0);
+    if (s[i] == c)
+    {
+        i++;
+        words--;
+    }
+    while (s[i] != '\0')
+    {
+        if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+            words++;
+        i++;
+    }
+    words++;
+    return (words);
+}
+
+static char *make_string(char const *s, char c)
+{
+    char    *word;
+    int     i;
+
+    i = 0;
+    while (s[i] && s[i] != c)
+        i++;
+    word = (char *)malloc (sizeof(char) * (i + 1));
+    if (!word)
+        return (NULL);
+    i = 0;
+    while (s[i] && s[i] != c)
+    {
+        word[i] = s[i];
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    char    **result;
+    int     num_words;
+    int     i;
+
+    if (!s)
+        return (NULL);
+    i = 0;
+    num_words = count_words(s, c);
+    result = (char **)malloc (sizeof(char *) * (num_words + 1));
+    if (!result)
+        return (NULL);
+    while (*s)
+    {
+        while (*s && *s == c)
+            s++;
+        if (*s && *s !=c)
+        {
+            result[i] = make_string(s,c);
+            i++;
+            while (*s && *s !=c)
+                s++;;
+        }
+        
+        result[i] = make_string(s, c);
+        num_words--;
+        i++; 
+    }
+    result[i] = NULL;
+    return (result);
+
+}
+
+/*int main (void)
+{
+    char *str = "hola c dfhdlfjdl skgjd c ksfgdf";
+    printf("%d\n",count_words(str, 'c'));
 }
 */
