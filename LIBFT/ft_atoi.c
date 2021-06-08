4b02fec24ae99047a8ad293f2902170912d36ce7
rfisla <rubenfernandezisla@gmail.com>
2021-06-08 23:39:29 +0200

    modifying ft_atoi

--- LIBFT/ft_atoi.c
+++ LIBFT/ft_atoi.c
@@ -21,14 +21,16 @@ int	ft_atoi(const char *nptr)
 
 	i = 0;
 	mult = 1;
+	number = 0;
 	if (nptr[0] == '\0')
 		return (i);
-	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\f' || \
+			nptr[i] == '\r' || nptr[i] == '\n' || nptr[i] == '\v')
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
-			mult = -1;
+			mult *= -1;
 		i++;
 	}
 	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
