852e9068bdf5c71ad3710444d4acdda8ef185b97
rfisla <rubenfernandezisla@gmail.com>
2021-06-24 01:28:59 +0200

    main function in progress

--- GET NEXT LINE/get_next_line.c
+++ GET NEXT LINE/get_next_line.c
@@ -13,34 +13,75 @@
 #include "get_next_line.h"
 #include <fcntl.h> 
 
+void	ft_strclr(char *s)
+{
+	if (s)
+	{
+		while (*s)
+		{
+			*s = '\0';
+			s++;
+		}
+	}
+}
+
+char    *check_s(char *s, char **line)
+{
+    char    *p_n;
+
+    p_n = NULL;
+    if (s)
+    {
+        if ((p_n = ft_strchr(s, '\n')))
+        {
+            *p_n = '\0';
+            *line = strdup(s);
+            strcpy(s, ++p_n);
+        }
+        else
+        {
+            *line = strdup(s);
+            ft_strclr(s);
+        }
+    }
+    else
+        *line = ft_strnew(1);
+    return (p_n);
+}
 int get_next_line (int fd, char **line)
 {
     static char *s;
     char        buf[BUFFER_SIZE + 1];
     char        *p_n;
     int         read_bytes;
-    char        *tmp;
+    char        *tmp; //? sin usar esta variable mismo resultado
 
-    p_n = NULL;
-    if (s)
-        *line = ft_strdup(s);
-    else
-        *line = ft_strnew(1);
-    while (!p_n && (read_bytes = (fd, buf, BUFFER_SIZE)))
+    if (fd < 0 || BUFFER_SIZE <= 0 || !line)
+        return (-1);
+    p_n = check_s(s, line);
+    while (!p_n && (read_bytes = read(fd, buf, BUFFER_SIZE))> 0)
     {
+        if (read_bytes == -1)
+            return (-1);
         buf[read_bytes] = '\0';
         if ((p_n = ft_strchr(buf, '\n')))
         {
             *p_n = '\0';
             p_n++;
             s = ft_strdup(p_n);
+        }
+        tmp = *line;
+        *line = ft_strjoin(*line, buf);
+        free(tmp);
+    }
+    if (read_bytes == 0)
+        {
+            *line = ft_strjoin(*line, buf);
+            return (0);
 
         }
-    }
-    tmp = *line;
-    *line = ft_strjoin(*line, buf);
-    free(tmp);
-    return (0);
+
+    return (1);
 }
 
 
@@ -49,14 +90,10 @@ int main (void)
     char *line;
     int fd;
     fd = open("test.txt", O_RDONLY);
-    get_next_line(fd, &line);
-    printf("%s\n\n", line);
-    
-    get_next_line(fd, &line);
-    printf("%s\n\n", line);
 
-    while(get_next_line(fd, &line))
-        printf("%s\n\n", line);
+
+    while (get_next_line(fd, &line))
+        printf("%s\n", line);
     
     return (0);
 
