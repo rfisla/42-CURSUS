caa5e4c9b385d00b5d452a2042c56230b33e9f16
Ruben Fernandez isla <rufernan@c1r16s4.42madrid.com>
2021-06-30 20:17:13 +0200

    correcting memory leaks

--- GET NEXT LINE/get_next_line.c
+++ GET NEXT LINE/get_next_line.c
@@ -13,92 +13,122 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 
-static int  get_line(char **s, char **line)
+static int  get_line(char **line, char **s, ssize_t read_bytes)
 {
-    int     len;
-    char    *temp;
+    char *temp;
+    char *temp2;
 
-    len = 0;
-    
-    while ((*s)[len] != '\n' && (*s)[len] != '\0')
-        len++;
-    if ((*s)[len] == '\n')
+    if (read_bytes < 0)
     {
-        *line = ft_substr(*s, 0, len);
-        temp = ft_strdup(&((*s)[len + 1]));
-        free(*s);
-        *s = temp;
-        if ((*s)[0] == '\0')
+        if (*s != NULL)
         {
-            free(*s);
+            free (*s);
             *s = NULL;
         }
+        return (-1);
     }
-    else if((*s)[len] == '\0')
+    if (!read_bytes && !*s)
     {
+        *line = ft_strdup("");
+        return (0);
+    }
+    if ((temp = ft_strchr(*s, '\n')))
+    {
+        *temp = '\0';
         *line = ft_strdup(*s);
+        temp2 = ft_strdup(temp + 1);
         free(*s);
+        *s = temp2;
+        return (1);
+    }
+    if ((ft_strchr(*s, '\0')))
+    {
+        *line = ft_strdup (*s);
+        free (*s);
         *s = NULL;
     }
-    return (1);
-}
-
-static int  output (char **s, char **line, int read_bytes, int fd)
-{
-   if (read_bytes < 0)
-        return (-1);
-    else if (read_bytes == 0 && s[fd] == NULL)
-        return (0);
-    else 
-        return (get_line(&s[fd], line));
+    return (0);
 }
 
 int get_next_line (int fd, char **line)
 {
     static char *s[4096];
-    char        *temp;
-    char        buf[BUFFER_SIZE];
-    int         read_bytes;
+    char        *buf;
+    ssize_t     read_bytes;
 
-    if (fd < 0 || line == NULL || BUFFER_SIZE <= 0)
+    read_bytes = 0;
+    if (fd < 0 || line == NULL || BUFFER_SIZE <= 0 ||
+		(!(buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1)))))
         return (-1);
-    while ((read_bytes = read(fd, buf, BUFFER_SIZE)))
+    while ((read_bytes = read(fd, buf, BUFFER_SIZE)) > 0)
     {
         buf[read_bytes] = '\0';
         if (!s[fd])
             s[fd] = ft_strdup(buf);
         else 
-        {
-            temp = ft_strjoin(s[fd], buf);
-            free(s[fd]);
-            s[fd] = temp;
-        }
-        if (ft_strchr(s[fd], '\n'))
+            s[fd] = ft_strjoin(s[fd], buf);
+        if (ft_strchr(buf, '\n'))
             break ;
-    }  
-    return (output (s, line, read_bytes, fd)); 
- 
+    }
+    free (buf);
+    buf= NULL;
+    return (get_line (line, &s[fd], read_bytes)); 
+}
+/*
+int main (void)
+{
+	int		fd;
+	char	*line;
+	ssize_t		ret;
+
+	fd = open("test.txt", O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) != 0)
+	{
+		printf("%s\n", line);
+	}
+	//printf("%s\n", line);
+    close(fd);
 }
 
-
+*/
+/*
 int main (void)
 {
     char *line;
     int fd;
+    //int fd2;
     fd = open("test.txt", O_RDONLY);
 
 
-    while (get_next_line(fd, &line))
+    while (get_next_line(fd, &line) > 0)
     {
-        printf("%s\n\n", line);
+        printf("%s\n", line);
     }
     close(fd);
-    fd = open("test.txt", O_RDONLY);
+    
+    fd2 = open("test.txt", O_RDONLY);
 
-    while (get_next_line(fd, &line))
+    while (get_next_line(fd2, &line))
     {
-        printf("%d\n", get_next_line(fd, &line));
+        printf("%d\n", get_next_line(fd2, &line));
     }
+    close(fd2);
 
     return (0);
 }
+*/
+/*
+int	main()
+{
+	int		fd;
+	char	*line;
+	fd = open("test.txt", O_RDONLY);
+	while (((get_next_line(fd, &line)) > 0))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+}
+*/
