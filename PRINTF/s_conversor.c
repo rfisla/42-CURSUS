ef952e0287829c1f3bdb083112e4c9f36d7885f1
Ruben Fernandez isla <rufernan@c1r13s6.42madrid.com>
2021-08-06 19:16:55 +0200

    improving s_conversor

--- PRINTF/s_conversor.c
+++ PRINTF/s_conversor.c
@@ -12,80 +12,82 @@
 
 #include "ft_printf.h"
 
-void    notdash_width_precission(t_printf *tab, char *str)
+void	common_exception(t_printf *tab, char *str)
 {
-    if (tab->point && !tab->precission)
-    {
-        if (tab->width)
-            filling_width(tab->width, tab);
-        else
-            write(1, "", 1);
-    }
-    if (tab->width > ft_strlen(str))
-    {
-        if (tab->precission && tab->precission <= ft_strlen(str))
-        {
-            filling_width(tab->width - tab->precission, tab);
-            tab->lenght += write(1, str, tab->precission);
-        }
-        else
-        {
-            filling_width(tab->width - ft_strlen(str), tab);
-            tab->lenght += write(1, str, ft_strlen(str));
-        }
-    }
-    else if ((!tab->width || tab->width <= ft_strlen(str)) && \
-            tab->precission > ft_strlen(str))
-            tab->lenght += write(1, str, ft_strlen(str));
-    else if ((!tab->width || tab->width <= ft_strlen(str)) && \
-            tab->precission && tab->precission<= ft_strlen(str))
-            tab->lenght += write(1, str, tab->precission);
+	if (tab->width)
+		filling_width(tab->width, tab);
+	else
+		write(1, "", 1);
 }
 
-void    dash_width_precission(t_printf *tab, char *str)
+void	notdash_width_precission(t_printf *tab, char *str)
 {
-    if (tab->point && !tab->precission)
-    {
-        if (tab->width)
-            filling_width(tab->width, tab);
-        else
-            write(1, "", 1);
-    }
-    if (tab->width > ft_strlen(str))
-    {
-        if (tab->precission && tab->precission <= ft_strlen(str))
-        {
-            tab->lenght += write(1, str, tab->precission);
-            filling_width(tab->width - tab->precission, tab);
-        }
-        else
-        {
-            tab->lenght += write(1, str, ft_strlen(str));
-            filling_width(tab->width - ft_strlen(str), tab);
-        }
-    }
-    else if ((!tab->width || tab->width <= ft_strlen(str)) && \
-            tab->precission > ft_strlen(str))
-        tab->lenght += write(1, str, ft_strlen(str));
-    else if ((!tab->width || tab->width <= ft_strlen(str)) && \
-            tab->precission && tab->precission <= ft_strlen(str))
-            tab->lenght += write(1, str, tab->precission);
-    else if ((!tab->width || tab->width <= ft_strlen(str)) && \
-            tab->point && tab->precission)
-        tab->lenght += write(1, str, ft_strlen(str));
+	if (tab->point && !tab->precission)
+		common_exception(tab, str);
+	else if (tab->width > ft_strlen(str))
+	{
+		if (tab->precission && tab->precission <= ft_strlen(str))
+		{
+			filling_width(tab->width - tab->precission, tab);
+			tab->lenght += write(1, str, tab->precission);
+		}
+		else
+		{
+			filling_width(tab->width - ft_strlen(str), tab);
+			tab->lenght += write(1, str, ft_strlen(str));
+		}
+	}
+	else if ((!tab->width || tab->width <= ft_strlen(str)) && \
+			tab->precission > ft_strlen(str))
+		tab->lenght += write(1, str, ft_strlen(str));
+	else if ((!tab->width || tab->width <= ft_strlen(str)) && \
+			tab->precission && tab->precission <= ft_strlen(str))
+		tab->lenght += write(1, str, tab->precission);
 }
 
-void    s_conversor(t_printf *tab)
+void	dash_width_precission(t_printf *tab, char *str)
 {
-    char    *str;
-    str = va_arg(tab->args, char *);
+	if (tab->point && !tab->precission)
+		common_exception(tab, str);
+	else if (tab->width > ft_strlen(str))
+	{
+		if (tab->precission && tab->precission <= ft_strlen(str))
+		{
+			tab->lenght += write(1, str, tab->precission);
+			filling_width(tab->width - tab->precission, tab);
+		}
+		else
+		{
+			tab->lenght += write(1, str, ft_strlen(str));
+			filling_width(tab->width - ft_strlen(str), tab);
+		}
+	}
+	else if ((!tab->width || tab->width <= ft_strlen(str)) && \
+			tab->precission > ft_strlen(str))
+		tab->lenght += write(1, str, ft_strlen(str));
+	else if ((!tab->width || tab->width <= ft_strlen(str)) && \
+			tab->precission && tab->precission <= ft_strlen(str))
+		tab->lenght += write(1, str, tab->precission);
+	else if ((!tab->width || tab->width <= ft_strlen(str)) && \
+			tab->point && tab->precission)
+		tab->lenght += write(1, str, ft_strlen(str));
+}
 
-    if (!str)
-        write (1, "", 1);
-    if ((!tab->width && !tab->precission) && !tab->point)
-        tab->lenght += write(1, str, ft_strlen(str));
-    else if (!tab->dash)
-        notdash_width_precission(tab, str);
-    else //if (tab->dash)
-        dash_width_precission(tab, str);
+void	s_conversor(t_printf *tab)
+{
+	char	*str;
+
+	str = va_arg(tab->args, char *);
+	if (!str)
+		write (1, "", 1);
+	if (tab->plus)
+		tab->plus = 0;
+	if (tab->zero && tab->dash)
+		tab->zero = 0;
+	if ((!tab->width && !tab->precission) && !tab->point)
+		tab->lenght += write(1, str, ft_strlen(str));
+	else if (!tab->dash)
+		notdash_width_precission(tab, str);
+	else
+		dash_width_precission(tab, str);
 }
