920e8489cd001de95f714669d9ea9ed877fbc133
rfisla <rubenfernandezisla@gmail.com>
2021-10-25 18:10:51 +0200

    fixing errors

--- PUSH_SWAP/short_lists_sorter.c
+++ PUSH_SWAP/short_lists_sorter.c
@@ -16,7 +16,7 @@ static void	sort_three(t_stack **stack_a)
 		(*stack_a)->next->next->number < (*stack_a)->number)
 	{
 		swap_a(stack_a);
-		reverse_rotate_a(stack_a, 'a');
+		reverse_rotate(stack_a, 'a');
 	}
 	else if ((*stack_a)->number > (*stack_a)->next->number && \
 		(*stack_a)->next->number < (*stack_a)->next->next->number && \
@@ -32,22 +32,24 @@ static void	sort_three(t_stack **stack_a)
 	else if ((*stack_a)->number < (*stack_a)->next->number && \
 		(*stack_a)->next->number > (*stack_a)->next->next->number && \
 		(*stack_a)->next->next->number < (*stack_a)->number)
-		reverse_rotate_a(stack_a, 'a');
+		reverse_rotate(stack_a, 'a');
 }
 
-static void	sort_four(t_stack **stack_a, t_stack **stack_b, int size)
+static void	sort_four(t_stack **stack_a, t_stack **stack_b)
 {
-	int	bingo;
+	int		bingo;
+	t_stack	*min;
 
 	bingo = 0;
+	min = hold_first(stack_a);
 	while (!bingo)
 	{
-		if (min_finder(stack_a, size))
+		if ((*stack_a)->number == min->number)
 		{
 			push_b(stack_a, stack_b);
 			bingo++;
 		}
-		else if (min_finder(&(*stack_a)->next, size - 1 - bingo))
+		else if ((*stack_a)->next->number == min->number)
 		{	
 			swap_a(stack_a);
 			push_b(stack_a, stack_b);
@@ -60,19 +62,21 @@ static void	sort_four(t_stack **stack_a, t_stack **stack_b, int size)
 	push_a(stack_a, stack_b);
 }
 
-static void	sort_five(t_stack **stack_a, t_stack **stack_b, int size)
+static void	sort_five(t_stack **stack_a, t_stack **stack_b)
 {
-	int	bingo;
+	int		bingo;
+	t_stack	*min;
 
 	bingo = 0;
 	while (bingo < 2)
 	{
-		if (min_finder(stack_a, size))
+		min = hold_first(stack_a);
+		if ((*stack_a)->number == min->number)
 		{
 			push_b(stack_a, stack_b);
 			bingo++;
 		}
-		else if (min_finder(&(*stack_a)->next, size - 1 - bingo))
+		else if ((*stack_a)->next->number == min->number)
 		{	
 			swap_a(stack_a);
 			push_b(stack_a, stack_b);
@@ -93,7 +97,7 @@ void	short_list_sorter(t_stack **stack_a, t_stack **stack_b, int size)
 	else if (size == 3)
 		sort_three(stack_a);
 	else if (size == 4)
-		sort_four(stack_a, stack_b, size);
+		sort_four(stack_a, stack_b);
 	else if (size == 5)
-		sort_five(stack_a, stack_b, size);
+		sort_five(stack_a, stack_b);
 }
